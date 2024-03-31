9d181209e597b067643cd32fe0d22d4f7abfb266
Carolina Nichita <carolinanichita89@gmail.com>
2024-03-31 11:37:45 +0200

    setup project

---
+++ src/main/java/cn/spring_data/project/SpringDataApplication.java
@@ -0,0 +1,13 @@
+package cn.spring_data.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+}
