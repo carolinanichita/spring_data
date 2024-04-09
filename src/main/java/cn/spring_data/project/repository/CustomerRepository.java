2492bc5639e0b073087c7f406d4bedabb48bd699
Carolina Nichita <carolinanichita89@gmail.com>
2024-04-09 12:49:26 +0200

    created class and interface projections

--- src/main/java/cn/spring_data/project/repository/CustomerRepository.java
+++ src/main/java/cn/spring_data/project/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package cn.spring_data.project.repository;
 
 import cn.spring_data.project.entity.Customer;
 import cn.spring_data.project.entity.projection.CustomerFullName;
+import cn.spring_data.project.entity.projection.CustomerInfo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,18 +12,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long>, CustomCustomerRepository {
-
-    List<Customer> findAllByFirstName(String firstName);
-
-    Optional<Customer> findFirstByLastNameOrderByFirstNameDesc(String lastName);
-
-    Customer findFirstByOrderByFirstNameAsc();
-
-    Stream<Customer> readAllByOrderByIdDesc();
-
-    @Query("select c from Customer c order by c.id asc limit 1")
-    Customer fetchFirstCustomerUsingQuery();
-
-    Customer searchUsingNamedQuery(String lastName);
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+    CustomerFullName findFirstByOrderByFirstNameDesc();
+    CustomerInfo findFirstByOrderByLastNameAsc();
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customer (id, first_name, last_name) values (nextval('customer_seq'), 'John', 'Smith');
+insert into customer (id, first_name, last_name) values (nextval('customer_seq'), 'Thomas', 'Edison');
+insert into customer (id, first_name, last_name) values (nextval('customer_seq'), 'Albert', 'Einstein');
