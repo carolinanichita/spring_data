8924237cb4ea57883a4296dfdca7646d2a48a181
Carolina Nichita <carolinanichita89@gmail.com>
2024-04-09 13:32:54 +0200

    querydsl extension

--- src/main/java/cn/spring_data/project/SpringDataApplication.java
+++ src/main/java/cn/spring_data/project/SpringDataApplication.java
@@ -1,18 +1,16 @@
 package cn.spring_data.project;
 
-import cn.spring_data.project.entity.Customer;
-import cn.spring_data.project.entity.events.CustomerCreationEvent;
+import cn.spring_data.project.entity.QCustomer;
 
 import cn.spring_data.project.repository.CustomerRepository;
-import jakarta.transaction.Transactional;
+
+import com.querydsl.core.types.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
@@ -26,20 +24,13 @@ public class SpringDataApplication implements CommandLineRunner {
 		SpringApplication.run(SpringDataApplication.class, args).close();
 	}
 
-	// listener method to handle the CustomerCreationEvent using the After Commit transaction phase
-	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-	public void handleCustomerCreationEvent(CustomerCreationEvent event) {
-		log.info("Handled CustomerCreationEvent...");
-	}
-
 	@Override
-	@Transactional
 	public void run(String... strings) throws Exception {
-		Customer customer1 = new Customer();
-		customer1.setFirstName("Carolina");
-		customer1.setLastName("Nichita");
-		customer1.afterSave();
-		customerRepository.save(customer1);
-	}
+		//query dsl predicate
+		QCustomer customer = QCustomer.customer;
+		Predicate predicate = customer.firstName.equalsIgnoreCase("thomas")
+				.and(customer.lastName.startsWithIgnoreCase("edi"));
 
+		log.info(String.valueOf(customerRepository.findAll(predicate)));
+	}
 }
